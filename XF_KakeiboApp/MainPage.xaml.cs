bc46453f7c60256c6243d570400109288573f228
Takahiro901 <30286581+Takahiro901@users.noreply.github.com>
2018-11-17 10:33:34 +0900

    費目一覧を追加

--- XF_KakeiboApp/MainPage.xaml.cs
+++ XF_KakeiboApp/MainPage.xaml.cs
@@ -9,8 +9,7 @@ namespace XF_KakeiboApp
     {
         public static List<string> list = new List<string>
         {
-            "食費",
-            "生活費"
+            "食費", "医療","美容院","交通費","勉強","生活費","交際費","趣味"
         };
 
 
