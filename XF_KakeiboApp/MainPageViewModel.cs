4e40547e58d6e0cbea51aa4eafa19f998a1e3831
Takahiro901 <30286581+Takahiro901@users.noreply.github.com>
2019-07-15 23:56:19 +0900

    ViewModelを追加

---
+++ XF_KakeiboApp/MainPageViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+using XF_KakeiboApp.Models;
+
+namespace XF_KakeiboApp
+{
+    public class MainPageViewModel
+    {
+        public MainPageViewModel()
+        {
+
+        }
+    }
+}
